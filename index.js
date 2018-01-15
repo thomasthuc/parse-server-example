0cd15994fe5b96f324c4743ac5ed039547994982
Thuc Phan <thuc.phan@bstarsolutions.com>
2018-01-15 15:02:45 +0700

    revert code

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'eahx2mXn8qvKe7Ol3XN21388XS8v9IR2',
+  masterKey: process.env.MASTER_KEY || '2l51DrpWdZ98h6O3D7OX8SZObW4EI17O', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
@@ -49,23 +49,29 @@ app.get('/test', function(req, res) {
 // Config dashboard
 var allowInsecureHTTP = true
 
-var dashboard = new ParseDashboard({
-  "apps": [
+
+var dashboardConfig = {
+  allowInsecureHTTP: true,
+  apps: [
     {
-      "serverURL": mountPath,
-      "appId": "myAppId",
-      "masterKey": "myMasterKey",
-      "appName": "ceo-connector"
+      serverURL: 	mountPath,
+      appId: 		'eahx2mXn8qvKe7Ol3XN21388XS8v9IR2',
+      masterKey: 	'2l51DrpWdZ98h6O3D7OX8SZObW4EI17O',
+      appName:          'ceo-connector',
     }
   ],
-  "users": [
+  users: [
     {
-      "user": "mobi",
-      "pass": "mobi"
+      user:"mobi",
+      pass:"mobi"
     }
   ]
-}, allowInsecureHTTP);
+}
 
+var dashboard = new ParseDashboard(
+  dashboardConfig,
+  true
+)
 app.use('/dashboard', dashboard);
 
 
